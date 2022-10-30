9e3699dca36aa27156cd5fdf5793d88de570d8ed
Nicolas <nicklimas2004@gmail.com>
2022-10-30 19:20:55 -0300

    Atualizando função set_senha_vazada

--- intruso.cpp
+++ intruso.cpp
@@ -1,18 +1,49 @@
 #include "intruso.hpp"
+#include <iostream>
 
 void Intruso::set_senha_vazada(std::string vazou){
+    std::string aux = tira_espaco_string(vazou);
     std::string senha;
-    for (auto &ch : vazou){
-        if (ch == 'A')
-            senha += vazou [0] + vazou [1];
-        if (ch == 'B')
-            senha += vazou [2] + vazou [3];
-        if (ch == 'C')
-            senha += vazou [4] + vazou [5];
-        if (ch == 'D')
-            senha += vazou [6] + vazou [7];
-        if (ch == 'E')
-            senha += vazou [8] + vazou [9];
+    for (auto &ch : aux){
+        if (ch == 'A'){
+            senha += aux [0];
+            senha += aux [1];
+            numero_de_caracteres++;
+        }
+        if (ch == 'B'){
+            senha += aux [2];
+            senha += aux [3];
+            numero_de_caracteres++;
+            }
+        if (ch == 'C'){ 
+            senha += aux [4];
+            senha += aux [5];
+            numero_de_caracteres++;
+            }
+        if (ch == 'D'){
+            senha += aux [6];
+            senha += aux [7];
+            numero_de_caracteres++;
+            }
+        if (ch == 'E'){
+            senha += aux [8];
+            senha += aux [9];
+            numero_de_caracteres++;
+            }
     }
+    std::cout << senha << std::endl;
     senhas.push_back(senha);
+}
+
+std::string Intruso::tira_espaco_string (std::string str){
+    std::string aux;
+    for (int i = 0; i < str.size(); i++){
+        if (str[i] != ' ')
+            aux += str[i];
+    }
+    return aux;
+}
+
+std::string Intruso::crack_senha(){
+   
 }
--- intruso.hpp
+++ intruso.hpp
@@ -1,11 +1,14 @@
 #include <string>
 #include <vector>
 
+
 class Intruso{
     /*Continue a implementação da classe Intruso*/
     std::vector <std::string> senhas;
+    int numero_de_caracteres = 0;
 
     public:
         void set_senha_vazada(std::string vazou);
         std::string crack_senha();
+        std::string tira_espaco_string (std::string str);
 };
